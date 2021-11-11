e3b99720945a40adb4a85b5da988dc1ee1659755
finecho <liuhao25@foxmail.com>
2021-11-11 18:34:30 +0800

    wip

--- src/Traits/ChainableHttpClient.php
+++ src/Traits/ChainableHttpClient.php
@@ -83,18 +83,18 @@ trait ChainableHttpClient
     {
         return [
             \preg_replace_callback(
-             '~\$(?<name>[a-z0-9_]+)~i',
-             callback: function ($matches) use (&$options) {
-                    if (empty($options[$matches['name']])) {
-                        throw new InvalidArgumentException(\sprintf('Missing url variables "%s".', $matches['name']));
-                    }
+                '~\$(?<name>[a-z0-9_]+)~i',
+                callback: function ($matches) use (&$options) {
+                 if (empty($options[$matches['name']])) {
+                     throw new InvalidArgumentException(\sprintf('Missing url variables "%s".', $matches['name']));
+                 }
 
-                    $value = $options[$matches['name']];
+                 $value = $options[$matches['name']];
 
-                    unset($options[$matches['name']]);
+                 unset($options[$matches['name']]);
 
-                    return $value;
-                },
+                 return $value;
+             },
                 subject: $uri
             ),
             $options,
